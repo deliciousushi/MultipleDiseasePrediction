ee0bb39b9ecb55e7c422ca64c95bb5daa8894149
Sushi <132209731+deliciousushi@users.noreply.github.com>
2024-11-11 18:14:00 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -305,7 +305,7 @@ if selected == "General Blood Test":
       user_input = np.array([float(x) for x in user_input]).reshape(1, -1)
     
       # Make a prediction
-      prediction = model.predict(user_input)[0]
+      prediction = bloodTest_model.predict(user_input)[0]
 
       # Interpret the prediction based on encoding
       if prediction == 2:
