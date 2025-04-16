8395082f8a7f2125657c3a4e4141fea3fee30ad4
Sushi <132209731+deliciousushi@users.noreply.github.com>
2025-04-16 22:40:27 +0530

    Update 8thsem1.py

--- 8thsem1.py
+++ 8thsem1.py
@@ -213,7 +213,7 @@ if selected == "Kidney Disease Prediction":
             prediction = kidney_model.predict(user_input)[0]
             if prediction == 1:
                 st.error("The person has kidney disease.")
-                show_doctor_booking("Nephrologist",doctor_data)
+                show_doctor_booking()
             else:
                 st.success("The person does not have kidney disease.")
         else:
